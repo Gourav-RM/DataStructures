2d4f9febcce672117eeea6f88bf76446a84c7599
Gourav Mishra <gouravmishra24121992@gmail.com>
2019-09-29 10:57:52 +0530

    Binary Search Tree API added

---
+++ DataStructures/src/Tree/BinarySearchTree.java
@@ -0,0 +1,147 @@
+package Tree;
+
+class BSTNode{
+	BSTNode left;
+	BSTNode right;
+	int data;
+	
+	BSTNode(int data){
+		this.data = data;
+	}
+
+	public BSTNode getLeft() {
+		return left;
+	}
+
+	public void setLeft(BSTNode left) {
+		this.left = left;
+	}
+
+	public BSTNode getRight() {
+		return right;
+	}
+
+	public void setRight(BSTNode right) {
+		this.right = right;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+	
+}
+
+public class BinarySearchTree {
+	
+	BSTNode root;
+	
+	public void insert(int data) {
+		root = insert(root,data);
+	}
+	private BSTNode insert(BSTNode node,int data) {
+		if(node == null)
+			node = new BSTNode(data);
+		else if(node.getData() > data)
+			node.left = insert(node.getLeft(),data);
+		else
+			node.right = insert(node.getRight(),data);
+		
+		return node;
+	}
+	public void inOrder() {
+		System.out.println("InOrder Traversal");
+		inOrder(root);
+		System.out.println();
+		System.out.println("---------------------------------------");
+	}
+	private void inOrder(BSTNode node) {
+		if(node == null) {
+			System.out.println("No data");
+		}else {
+			if(node.getLeft() != null)
+				inOrder(node.getLeft());
+			System.out.print(node.data+" ");
+			if(node.getRight() != null)
+				inOrder(node.getRight());
+		}
+	}
+	public void preOrder() {
+		System.out.println("PreOrder Traversal");
+		preOrder(root);
+		System.out.println();
+		System.out.println("---------------------------------------");
+	}
+	private void preOrder(BSTNode node) {
+		if(node == null) {
+			System.out.println("No data");
+		}else {
+			System.out.print(node.data+" ");
+			if(node.getLeft() != null)
+				preOrder(node.getLeft());
+			if(node.getRight() != null)
+				preOrder(node.getRight());
+		}
+	}
+	public void postOrder() {
+		System.out.println("PostOrder Traversal");
+		postOrder(root);
+		System.out.println();
+		System.out.println("---------------------------------------");
+	}
+	private void postOrder(BSTNode node) {
+		if(node == null) {
+			System.out.println("No data");
+		}else {
+			if(node.getLeft() != null)
+				postOrder(node.getLeft());
+			if(node.getRight() != null)
+				postOrder(node.getRight());
+			System.out.print(node.data+" ");
+		}
+	}
+	public boolean search(int data) {
+		return search(root,data);
+	}
+	private boolean search(BSTNode node,int data) {
+		if(node.getData() == data)
+			return true;
+		else if((node.getLeft() !=null) && (node.getData() > data)) {
+			if(search(node.getLeft(),data))
+				return true;
+		}else if((node.getRight() !=null) && (node.getData() < data)) {
+			if(search(node.getRight(),data))
+				return true;
+		}
+		return false;
+	}
+	public int countNodes() {
+		if(root != null)
+			return countNodes(root);
+		else
+			return 0;
+	}
+	private int countNodes(BSTNode node) {
+		int count = 0;
+		if(node != null)
+			count++;
+		if(node.getLeft() != null)
+			count += countNodes(node.getLeft());
+		if(node.getRight() != null)
+			count += countNodes(node.getRight());
+		
+		return count;
+	}
+	public boolean isEmpty() {
+		if(root == null)
+			return true;
+		else
+			return false;
+	}
+	public void clear() {
+		root = null;
+	}
+}
