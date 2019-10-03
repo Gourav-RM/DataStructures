040da6a4f841cd3bcbe7359ed8e2ec0113c439c1
Gourav Mishra <gouravmishra24121992@gmail.com>
2019-10-03 09:33:28 +0530

    traversal methods removed from child classes

--- DataStructures/src/Tree/MaxHeap.java
+++ DataStructures/src/Tree/MaxHeap.java
@@ -48,98 +48,6 @@ public class MaxHeap extends Tree{
 		return node;
 	}
 
-	public void inOrder() {
-		System.out.println("InOrder Traversal");
-		inOrder(root);
-		System.out.println();
-		System.out.println("---------------------------------------");
-	}
-
-	private void inOrder(Node node) {
-		if (node == null) {
-			System.out.println("No data");
-		} else {
-			if (node.getLeft() != null)
-				inOrder(node.getLeft());
-			System.out.print(node.data + " ");
-			if (node.getRight() != null)
-				inOrder(node.getRight());
-		}
-	}
-
-	public void preOrder() {
-		System.out.println("PreOrder Traversal");
-		preOrder(root);
-		System.out.println();
-		System.out.println("---------------------------------------");
-	}
-
-	private void preOrder(Node node) {
-		if (node == null) {
-			System.out.println("No data");
-		} else {
-			System.out.print(node.data + " ");
-			if (node.getLeft() != null)
-				preOrder(node.getLeft());
-			if (node.getRight() != null)
-				preOrder(node.getRight());
-		}
-	}
-
-	public void postOrder() {
-		System.out.println("PostOrder Traversal");
-		postOrder(root);
-		System.out.println();
-		System.out.println("---------------------------------------");
-	}
-
-	private void postOrder(Node node) {
-		if (node == null) {
-			System.out.println("No data");
-		} else {
-			if (node.getLeft() != null)
-				postOrder(node.getLeft());
-			if (node.getRight() != null)
-				postOrder(node.getRight());
-			System.out.print(node.data + " ");
-		}
-
-	}
-
-	public int countNodes() {
-		if (root != null)
-			return countNodes(root);
-		else
-			return 0;
-	}
-
-	private int countNodes(Node node) {
-		int count = 0;
-		if (node != null)
-			count++;
-		if (node.getLeft() != null)
-			count += countNodes(node.getLeft());
-		if (node.getRight() != null)
-			count += countNodes(node.getRight());
-
-		return count;
-	}
-
-	public boolean isEmpty() {
-		if (root == null)
-			return true;
-		else
-			return false;
-	}
-
-	public void clear() {
-		root = null;
-	}
-
-	public int height() {
-		return Trees.height(root);
-	}
-
 	public void delete() {
 		delete(heap, 0);
 	}
@@ -176,4 +84,22 @@ public class MaxHeap extends Tree{
 		}
 		heap = Arrays.copyOfRange(heap, 0, heap.length - 2);
 	}
+
+	public static void main(String[] args) {
+		MaxHeap maxHeap = new MaxHeap(new int[] { 10, 20, 30, 40, 34, 60 });
+		for (int i = 0; i < maxHeap.heap.length; i++) {
+			System.out.print(maxHeap.heap[i] + " ");
+		}
+		maxHeap.inOrder();
+		maxHeap.preOrder();
+		maxHeap.postOrder();
+		System.out.println(maxHeap.countNodes());
+		System.out.println(maxHeap.height());
+		System.out.println(maxHeap.isEmpty());
+		maxHeap.delete();
+		for (int i = 0; i < maxHeap.heap.length; i++) {
+			System.out.print(maxHeap.heap[i] + " ");
+		}
+	}
+
 }
