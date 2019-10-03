3f4d1e324ba54549082c993451f2a7361bae4b04
Gourav Mishra <gouravmishra24121992@gmail.com>
2019-10-03 23:14:34 +0530

    adding setters and getters to stack API

--- DataStructures/src/stack/Stack.java
+++ DataStructures/src/stack/Stack.java
@@ -5,28 +5,47 @@ public class Stack {
 	int[] stack;
 	int size;
 	int top;
+	
+	public int[] getStack() {
+		return stack;
+	}
+	public void setStack(int[] stack) {
+		this.stack = stack;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public int getTop() {
+		return top;
+	}
+	public void setTop(int top) {
+		this.top = top;
+	}
 	Stack(){
 		top = -1;
 	}
 	Stack(int[] stack){
 		this.stack = stack;
-		top = stack.length - 1;
-		size = top + 1;
+		setTop(getStack().length-1);
+		setSize(getTop()+1);
 	}
 	public boolean isEmpty() {
-		return top < 0 ? true : false;
+		return getTop() < 0 ? true : false;
 	}
 	public boolean isFull() {
-		return top == stack.length -1 ? true : false;
+		return getTop() == stack.length -1 ? true : false;
 	}
 	public void push(int data) {
 		if(isFull()) {
 			System.out.println("Tree is full");
 			return;
 		}else {
-			top += 1;
+			setTop(getTop()+1);
 			stack[top] = data;
-			size++;
+			setSize(getSize()+1);
 		}
 	}
 	public void pop() {
@@ -34,8 +53,8 @@ public class Stack {
 			System.out.println("Tree is empty");
 			return;
 		}else {
-			top = top - 1;
-			size--;
+			setTop(getTop()-1);
+			setSize(getSize()-1);
 		}
 	}
 	public int peek() {
@@ -47,13 +66,13 @@ public class Stack {
 		}
 	}
 	public int size() {
-		return size;
+		return getSize();
 	}
 	public void show() {
 		if(isEmpty()) {
 			System.out.println("Tree is empty");
 		}else {
-			for(int i=top;i>=0;i--) {
+			for(int i=getTop();i>=0;i--) {
 				System.out.println(stack[i]);
 			}
 		}
